e7bcdb4ab5bccd48911a730730230e4f824325e4
Sam Robbins <samrobbinsgb@gmail.com>
2021-12-17 14:27:27 +0000

    Switch to woff2

--- src/App.svelte
+++ src/App.svelte
@@ -58,7 +58,7 @@
   }
   @font-face {
     font-family: Christmas;
-    src: url(PWHappyChristmas.ttf);
+    src: url(PWHappyChristmas.woff2) format("woff2");
   }
   main {
     text-align: center;
